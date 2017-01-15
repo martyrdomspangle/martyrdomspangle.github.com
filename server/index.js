8f618e6d84995b9a9a7d5565e146b2be69f522f3
Igor Sholom <hairever@hotmail.com>
2017-01-15 02:46:38 +0200

    changed to new design

--- server/index.js
+++ server/index.js
@@ -1,12 +1,6 @@
 const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
 app.use(express.static('./'));
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  extended: true,
-}));
 
 app.listen(3000, (err) => {
   console.log(err || "Server started");
