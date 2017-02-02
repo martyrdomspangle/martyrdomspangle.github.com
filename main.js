512d586efc86875252f145683490cc7a09ebda63
bolein <bolein95@gmail.com>
2017-02-02 17:36:08 +0200

    changed way of page loading

--- main.js
+++ main.js
@@ -1,19 +1,33 @@
 $(document).ready(function () {
-  Promise.all($('header.menu a').map((_, anchor) => {
+  var menu = $('header.menu a');
+  Promise.all(menu.map((_, anchor) => {
     const pageName = $(anchor).attr('href').substring(1);
 
-    return $.get(`/pages/${pageName}.html`).then(html => $('.stripe').append(html));
-  })).then((...htmls) => htmls.forEach(html =>
-    $('.stripe').append(html)
-  ));
-
-  location.hash = location.hash || 'home';
-  $.get(`/pages/${location.hash.substring(1)}.html`).then(html => $('.wrapper').append(html));
-
-  $('header.menu a').click(function(e) {
-    const page = $(this).attr('href').substring(1);
-
-    $('.wrapper').html('');
-    $.get(`/pages/${page}.html`).then(html => $('.wrapper').append(html));
+    return $.get(`/pages/${pageName}.html`).then(html => $('.wrapper').append(html));
+  })).then(r => {
+    location.hash = location.hash || 'home';
+    setActive.call($('a[href="' + location.hash +'"]').first().get(), null, location.hash);
+    menu.click(setActive);
   });
 });
+
+function setActive(e, id) {
+  
+  console.log(this);
+
+  $('header.menu a').each(function () {
+    $(this).removeClass('selected');
+  });
+
+  $(this).addClass('selected');
+
+  // hide all
+  $('.wrapper > section').each(function () {
+    $(this).hide();
+  });
+
+  // show current
+  var page = id || $(this).attr('href');
+  console.log(page);
+  $(page).show();
+}
