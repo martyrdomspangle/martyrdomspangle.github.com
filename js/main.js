76ea5194c66ba343e744dcce5e049ec7e45e8c11
martyrdomspangle <argenuma@gmail.com>
2017-03-19 19:22:44 +0200

    Added scrolling when clicking on home page's buttons to appropriate sections

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@ $(document).ready(function () {
     menu.click(setActive);
   });
 
-    $('a[href*="#"]:not([href="#"])').click(function() {
+    $('body').on('click', 'a[href*="#"]:not([href="#"])', function() {
       if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
         var target = $(this.hash);
         target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
